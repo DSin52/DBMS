87e4f24b63e11e57596f521d35b21f0ef2d9bef9
Harsh Agrawal <h.agrawal092@gmail.com>
2014-11-10 12:27:45 -0500

    ipython notebook for extracting citation graph and resulting json file

---
+++ parse_database/parse_graph.py
@@ -0,0 +1,21 @@
+
+
+paper_graph = {}
+def readCitationFile(path):
+    with open(path,'r') as file:
+        for line in file:
+            paper = line.split(' ')
+            paper1 = str(paper[0])
+            paper2 = str(paper[1])
+            if paper1 in paper_graph:
+                paper_graph[paper1]['cited'].append(paper2)
+            else:
+                paper_graph[paper1] = {'cited':[paper2]}
+
+
+def readSLACFile(path):
+    with open(path, 'r') as file:
+        for line in file:
+            token = line.split(' ')
+            paper = str(token[0])
+            date = str(token[1])
